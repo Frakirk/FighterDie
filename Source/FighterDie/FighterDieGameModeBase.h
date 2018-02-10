b3627a7f4458b33f429067a2effeb315979354c4
Francis <frakirk@gmail.com>
2018-02-10 16:41:54 +1300

    First commit! This is where the game is currently at.

---
+++ Source/FighterDie/FighterDieGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FighterDieGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIGHTERDIE_API AFighterDieGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
